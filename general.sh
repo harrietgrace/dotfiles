c40f2050191d656937696bf208c4852fdd77055d
Harriet Lawrence <harrietgracelawrence@gmail.com>
2015-08-03 09:34:20 +1000

    Added terminal prompt styles

--- general.sh
+++ general.sh
@@ -37,3 +37,18 @@ _goto()
 }
 
 complete -o default -o nospace -F _goto goto
+
+#
+# Style for terminal prompt
+#
+
+function parse_git_branch () {
+  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}
+
+RED="\[\033[0;31m\]"
+YELLOW="\[\033[0;33m\]"
+GREEN="\[\033[0;32m\]"
+NO_COLOR="\[\033[0m\]"
+
+PS1="$GREEN\u$NO_COLOR:\W$YELLOW\$(parse_git_branch)$NO_COLOR\$ "
