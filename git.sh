53e8a62ebb80343d96a0270d23ba97ed047962eb
Harriet Lawrence <harrietgracelawrence@gmail.com>
2016-02-12 11:59:51 +1100

    Update git aliases

--- git.sh
+++ git.sh
@@ -1,29 +1,28 @@
 alias gd="git diff"
-alias gc="git clone"
 alias ga="git add"
 alias gbd="git branch -D"
 alias gs="git status"
-alias gst="git status"
 alias gss="git status -s"
-alias gca="git commit -a -m"
-alias gpt="git push --tags"
-alias gp="git push"
+alias gc="git commit"
+alias gcm="git commit -m"
+alias gp="git push origin"
 alias grh="git reset --hard"
 alias gb="git branch"
 alias gcob="git checkout -b"
 alias gm="git merge"
 alias gco="git checkout"
 alias gba="git branch -a"
-alias gcp="git cherry-pick"
-alias gl="git log --pretty='format:%Cgreen%h%Creset %an - %s' --graph"
-alias docs="rm -fr /tmp/docs && cp -fr docs /tmp/docs && git checkout gh-pages && cp -fr /tmp/docs/* ."
+alias gl="git log"
+alias glp="git log --pretty='format:%Cgreen%h%Creset %an - %s' --graph"
+alias gpo="git pull origin"
 alias gpom="git pull origin master"
-alias gpum="git pull upstream master"
-alias gcd='cd "`git rev-parse --show-toplevel`"'
-alias gdmb='git delete-merged-branches'
-alias gpr='git pull --rebase'
-alias gprm='git pull --rebase origin master'
-alias glm='git fetch; git checkout master; git pull origin master'
+alias gfcp='git fetch; git checkout master; git pull origin master'
+alias gri="git rebase -i"
+#alias gcd='cd "`git rev-parse --show-toplevel`"'
+#alias gdmb='git delete-merged-branches'
+#alias gpr='git pull --rebase'
+#alias gprm='git pull --rebase origin master'
+
 
 # default
 
