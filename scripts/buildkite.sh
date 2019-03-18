88ad37cfbf5f186163ec1004a66b1109074aa204
Harriet Lawrence <harrietgracelawrence@gmail.com>
2019-03-18 16:48:54 +0000

    Add new docs site aliases

--- scripts/buildkite.sh
+++ scripts/buildkite.sh
@@ -2,5 +2,10 @@
 alias ysite="cd ~/dev/buildkite/buildkite/vendor/site && yarn && cd ~/dev/buildkite/buildkite"
 alias yfrontend="cd ~/dev/buildkite/buildkite/vendor/frontend && yarn && cd ~/dev/buildkite/buildkite"
 alias ysubs="ysite && yfrontend"
+
 # run migrations
 alias migr="bin/rails db:migrate RAILS_ENV=development"
+
+# run the docs stuff
+alias tests="bundle && bundle exec rspec"
+alias launch="echo http://localhost:3000/ && bin/rails server"
