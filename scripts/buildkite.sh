cf025d659cb51a37f59176a4e73d60c64663e3cc
Harriet Lawrence <harrietgracelawrence@gmail.com>
2019-03-22 16:38:21 +0000

    Add alias for latest

--- scripts/buildkite.sh
+++ scripts/buildkite.sh
@@ -6,6 +6,9 @@ alias ysubs="ysite && yfrontend"
 # run migrations
 alias migr="bin/rails db:migrate RAILS_ENV=development"
 
+# run all the things
+alias latest="gpom && gf && gsubs && bundle && ysubs && migr"
+
 # run the docs stuff
 alias tests="bundle && bundle exec rspec"
 alias launch="echo http://localhost:3000/ && bin/rails server"
