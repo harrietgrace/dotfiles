86ece5487b53d87529b7c02cea613baa340f2cfb
Harriet Lawrence <harrietgracelawrence@gmail.com>
2020-07-09 11:22:32 +1000

    Add linting alias

--- scripts/buildkite.sh
+++ scripts/buildkite.sh
@@ -9,9 +9,10 @@ alias migr="bin/rails db:migrate RAILS_ENV=development"
 # run all the things
 alias latest="gco db/structure.sql && gpom && gf && gsubs && bundle && ysubs && migr"
 
-# run the docs stuff
+# run the docs stuff (assumes you're in the docs dir already)
 alias tests="bundle && bundle exec rspec"
 alias launch="echo http://localhost:3000/ && bin/rails server"
+alias lint="docker run --rm --volume $PWD:/workspace --workdir /workspace jdkato/vale --glob='!*.DS_Store' pages/"
 
 # go to things
 alias docs="cd ~/dev/buildkite/buildkite/vendor/docs"
